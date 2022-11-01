8cdab7ff12817de9ec5cab32dd5159a063dc6730
StinkyStonky <Brady.wynn12@gmail.com>
2022-11-01 15:52:22 -0400

    rescaled the xml files
    wrote a xml parser to convert the xml labels for the high resolution images down to 300x300

--- workspace/training_demo/xml_parse.py
+++ workspace/training_demo/xml_parse.py
@@ -1,10 +1,44 @@
+import cv2 as cv
+import xml.etree.ElementTree as ET
+import os
 
-from bs4 import BeautifulSoup
+directory = r'C:\ProgramData\Tensorflow\powerplay-tensorflow\workspace\training_demo\images\test'
+ 
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if f.endswith('xml'):
 
-with open(r'C:\ProgramData\robotics\TensorFlow\workspace\training_demo\images\train\7b061911-IMG_20220923_163153908.xml', 'r') as f:
-	file = f.read() 
+        mytree = ET.parse(f)
+        myroot = mytree.getroot()
 
-soup = BeautifulSoup(file, 'xml')
-names = soup.find_all('xmin')
-for name in names:
-    print(name.text)
+        for widths in myroot.iter('width'):
+            width = widths.text
+            widths.text = str(300)
+        for heights in myroot.iter('height'):
+            height = heights.text
+            heights.text = str(300)
+
+        xcomp = 300 / float(width)
+        ycomp = 300 / float(height)
+
+        print(xcomp)
+        print(ycomp)
+        
+        for xmins in myroot.iter('xmin'):
+            xmins.text = str(int(float(xmins.text) * xcomp))
+        for xmaxs in myroot.iter('xmax'):
+            xmaxs.text = str(int(float(xmaxs.text) * xcomp))
+        for ymins in myroot.iter('ymin'):
+            ymins.text = str(int(float(ymins.text) * ycomp))
+        for ymaxs in myroot.iter('ymax'):
+            ymaxs.text = str(int(float(ymaxs.text) * ycomp))
+
+        mytree.write(filename)
+        # break
+
+# img = cv.imread(r'C:\ProgramData\Tensorflow\powerplay-tensorflow\workspace\training_demo\images\train\0008709e-98.jpg')
+# buh = cv.rectangle(img, (174, 84), (198, 147), (0, 0, 0), 1)
+
+# cv.imshow("joe biden", buh)
+# cv.waitKey(0) 
+# cv.destroyAllWindows() 
